c910a02ce624d8afc8ad780af156d8ece7ba8931
Jason <47071224+JasonHalvorson@users.noreply.github.com>
2022-01-02 23:29:49 -0800

    Added a loading indicator on the creation button, and prevent spamming the button creating multiple notes

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,14 @@ import Color from '../components/Form/Color';
 import MarkdownCheatsheet from '../components/Form/MarkdownCheatsheet';
 import { colors } from '../lib/colors';
 import { addMinutes, addHours, addDays, formatISO } from 'date-fns';
+import { PencilAltIcon } from '@heroicons/react/solid';
+
+const LoadingIcon = () => (
+    <svg className="w-5 h-5 mr-3 animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>
+);
 
 const expiry = [
     { displayTime: '10 Minutes', computedTime: () => formatISO(addMinutes(new Date(), 10)) },
@@ -21,7 +29,7 @@ const expiry = [
 ];
 
 export default function Home() {
-    const [state, setState] = useState({ to: '', from: '', content: '' });
+    const [state, setState] = useState({ to: '', from: '', content: '', submitting: false });
     const [expiryTime, setExpiryTime] = useState(expiry[7]);
     const [color, setColor] = useState(Object.keys(colors)[0]);
     const [openCheatsheet, setOpenCheatsheet] = useState(false);
@@ -37,6 +45,8 @@ export default function Home() {
 
     const submitData = async (e) => {
         e.preventDefault();
+
+        setState((prevState) => ({ ...prevState, submitting: true }));
 
         const body = {
             to: state.to,
@@ -95,8 +105,9 @@ export default function Home() {
                     <Color onChange={setColor} value={color} />
                 </div>
 
-                <button disabled={state.to === '' || state.from === '' || state.content === ''} type="submit" className="mt-10 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:bg-indigo-400">
-                    Create Note
+                <button disabled={state.to === '' || state.from === '' || state.content === '' || state.submitting} type="submit" className="mt-10 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:bg-indigo-400 w-40">
+                    {state.submitting ? <LoadingIcon /> : <PencilAltIcon className="w-5 h-5 mr-3" />}
+                    <span className="ml-auto">{state.submitting ? 'Creating...' : 'Create Note'}</span>
                 </button>
             </form>
             <MarkdownCheatsheet open={openCheatsheet} setOpen={setOpenCheatsheet} />

2d3dbeab7c5570a4a21f3da133ed660d03201cc9
Jason <47071224+JasonHalvorson@users.noreply.github.com>
2022-03-11 22:52:46 -0800

    Add Ackee

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import '../styles/globals.css';
+import useAckee from 'use-ackee';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+    const { pathname } = useRouter();
+
+    useAckee(pathname, {
+        server: 'https://ackee.dev.jasonhalvorson.ca',
+        domainId: '32b42107-f5f7-4620-a55c-1fd43e8b10e3',
+    });
+
     return <Component {...pageProps} />;
 }
 
